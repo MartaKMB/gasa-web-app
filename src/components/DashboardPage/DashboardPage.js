ef3c0511381b6e8d3f507702d6313a35249ac874
MartaKMB <muszkamarta@interia.pl>
2023-07-17 11:23:57 +0200

    added some styles to DashboardPage

--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -18,14 +18,14 @@ const DashboardPage = () => {
     return (
         <div className='dashboard-page'>
             <section className="dashboard-page__container">
-                <div className="dashboard-page__welcome-dashboard-section">
-                    <h3>{`Cześć ${userNameForApp}!`}</h3>
-                    <h4>Witaj w aplikacji, dzięki której stworzysz rytuał relaksu dopasowany do Ciebie i Twojego Czworonoga.</h4>
+                <div className="dashboard-page__welcome-dashboard-section welcome-dashboard-section">
+                    <h3 className="welcome-dashboard-section__hello-txt">{`Cześć ${userNameForApp}!`}</h3>
+                    <h4 className="welcome-dashboard-section__app-description-txt">Witaj w aplikacji, dzięki której stworzysz rytuał relaksu dopasowany do Ciebie i Twojego Czworonoga.</h4>
                     <DogPaw />
                 </div>
                 <ButtonComponent name='rozpocznij' handleOnClick={handleOnStart}/>
-                <div className="dashboard-page__rules-dashboard-section">
-                    Główne założenia GaSa to:
+                <div className="dashboard-page__rules-dashboard-section rules-dashboard-section">
+                    <span className="rules-dashboard-section__rules-title">Główne założenia GaSa to:</span>
                     <ul>
                         <li>rozmowa (nie zawsze) poprzez dotyk</li>
                         <li>komfort zarówno dawcy jak i biorcy</li>
