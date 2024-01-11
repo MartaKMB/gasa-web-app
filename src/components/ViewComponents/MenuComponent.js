923ea01cf3a9e8ffe267ef0123121b639d1a38ea
MartaKMB <muszkamarta@interia.pl>
2024-01-11 17:04:22 +0100

    positioning menu with animation

--- src/components/ViewComponents/MenuComponent.js
+++ src/components/ViewComponents/MenuComponent.js
@@ -1,29 +1,39 @@
+import { useState } from 'react';
+
 import { AiOutlineMenu } from 'react-icons/ai';
 import { IconContext } from 'react-icons';
 
 import appCircle from '../../assets/pngImages/circle.png';
 
 const MenuComponent = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  
+  const menuAnimation = {left: isMenuVisible && '0'};
+
+  const handleMenuIconClick = () => setIsMenuVisible(!isMenuVisible);
+
   return (
-    <nav className='gasa-app-menu-container'>
-      <div className='gasa-app-menu-container__menu-bar menu-bar'>
-        <IconContext.Provider value={{ size: '1.5em', color: '#2b2b2b' }}>
-          <AiOutlineMenu />
-        </IconContext.Provider>
-        <ul className='gasa-app-menu-container__menu-list menu-list'>
-          <li>START</li>
-          <li>PRZYKŁADOWE SESJE</li>
-          <li>PODĄŻAJ ZA PSEM</li>
-          <li>USTAWIENIA</li>
-          <li>KONTAKT</li>
-        </ul>
-        <img
-          src={appCircle}
-          alt='kółko aplikacji'
-          className='menu-bar__circle'
-        />
-      </div>
-    </nav>
+    <>
+      <nav className='gasa-app-menu-container'>
+        <div className='gasa-app-menu-container__menu-bar menu-bar'>
+          <IconContext.Provider value={{ size: '1.5em', color: '#2b2b2b' }}>
+            <AiOutlineMenu onClick={handleMenuIconClick} />
+          </IconContext.Provider>
+          <img
+            src={appCircle}
+            alt='kółko aplikacji'
+            className='menu-bar__circle'
+          />
+        </div>
+      </nav>
+      <ul className='gasa-app-menu-list' style={menuAnimation}>
+        <li>START</li>
+        <li>PRZYKŁADOWE SESJE</li>
+        <li>PODĄŻAJ ZA PSEM</li>
+        <li>USTAWIENIA</li>
+        <li>KONTAKT</li>
+      </ul>
+    </>
   );
 };
 
