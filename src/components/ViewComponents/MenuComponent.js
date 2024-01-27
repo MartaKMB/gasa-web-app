6ee62572c48e034311e6fce7ab160fb323d33ff2
MartaKMB <muszkamarta@interia.pl>
2024-01-27 11:52:36 +0100

    add new icon for menu and start work at click outside menu

--- src/components/ViewComponents/MenuComponent.js
+++ src/components/ViewComponents/MenuComponent.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineMenuFold, AiOutlineMenuUnfold } from 'react-icons/ai';
 import { IconContext } from 'react-icons';
 
 import appCircle from '../../assets/pngImages/circle.png';
@@ -9,6 +9,8 @@ import { navigateTo } from '../../utils/navigateToOwnerChoice';
 
 const MenuComponent = () => {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
+  let navigate = useNavigate();
+
   const ownerChoiceArray = [
     'START_PAGE',
     'SESSIONS_CHOICE',
@@ -16,9 +18,24 @@ const MenuComponent = () => {
     'SETTINGS',
     'CONTACT',
   ];
-  
-  const menuAnimation = {left: isMenuVisible && '0'};
-  let navigate = useNavigate();
+
+  // WIP: click outside menu
+  // let menuRef = useRef();
+  // useEffect(() => {
+  //   let handler = (e) => {
+  //     if(menuRef.current.contains(e.target)) {
+  //       setIsMenuVisible(false);
+  //       console.log(menuRef.current);
+  //     }
+  //   };
+  //   document.addEventListener('mousedown', handler);
+
+  //   return() => {
+  //     document.removeEventListener('mousedown', handler);
+  //   };
+  // });
+
+  const menuAnimation = { left: isMenuVisible && '0' };
 
   const handleMenuIconClick = () => setIsMenuVisible(!isMenuVisible);
 
@@ -33,20 +50,31 @@ const MenuComponent = () => {
       <nav className='gasa-app-menu-container'>
         <div className='gasa-app-menu-container__menu-bar menu-bar'>
           <IconContext.Provider value={{ size: '1.5em', color: '#2b2b2b' }}>
-            <AiOutlineMenu onClick={handleMenuIconClick} />
+            {isMenuVisible ? (
+              <AiOutlineMenuFold onClick={handleMenuIconClick} />
+            ) : (
+              <AiOutlineMenuUnfold onClick={handleMenuIconClick} />
+            )}
           </IconContext.Provider>
           <img
             src={appCircle}
             alt='kółko aplikacji'
             className='menu-bar__circle'
+            onClick={(e) => handleOnChoice(e, ownerChoiceArray[0])}
           />
         </div>
       </nav>
       <ul className='gasa-app-menu-list' style={menuAnimation}>
         <li onClick={(e) => handleOnChoice(e, ownerChoiceArray[0])}>START</li>
-        <li onClick={(e) => handleOnChoice(e, ownerChoiceArray[1])}>PRZYKŁADOWE SESJE</li>
-        <li onClick={(e) => handleOnChoice(e, ownerChoiceArray[2])}>PODĄŻAJ ZA PSEM</li>
-        <li onClick={(e) => handleOnChoice(e, ownerChoiceArray[3])}>USTAWIENIA</li>
+        <li onClick={(e) => handleOnChoice(e, ownerChoiceArray[1])}>
+          PRZYKŁADOWE SESJE
+        </li>
+        <li onClick={(e) => handleOnChoice(e, ownerChoiceArray[2])}>
+          PODĄŻAJ ZA PSEM
+        </li>
+        <li onClick={(e) => handleOnChoice(e, ownerChoiceArray[3])}>
+          USTAWIENIA
+        </li>
         <li onClick={(e) => handleOnChoice(e, ownerChoiceArray[4])}>KONTAKT</li>
       </ul>
     </>
